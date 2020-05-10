879b943ba0f37d6da63692c105e1b4168c381ab4
Ming Qu <ming.qu0202@gmail.com>
2020-05-10 23:50:40 +0200

    add tree

--- Tree-Easy/104.py
+++ Tree-Easy/104.py
@@ -14,3 +14,16 @@ class Solution:
             return max(helper(node.left, s + 1), helper(node.right, s + 1))
 
         return helper(root, 1)
+
+    def maxDepth_i(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        stack = [[root, 1]]
+        h = 0
+        m = 0
+        while len(stack):
+            top, h = stack.pop()
+            if top.left: stack.append([top.left, h + 1])
+            if top.right: stack.append([top.right, h + 1])
+            m = max(h, m)
+        return m
