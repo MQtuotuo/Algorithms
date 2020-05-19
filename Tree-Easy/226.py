ed75a0e1c27f6957446ff256a2bc3cc3779cbe14
Ming Qu <ming.qu0202@gmail.com>
2020-05-19 23:31:39 +0200

    one more tree

---
+++ Tree-Easy/226.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree_i(self, root: TreeNode) -> TreeNode:
+        if root == None: return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
+        return root
+
+    def invertTree_r(self, root: TreeNode) -> TreeNode:
+
+        def rec(root):
+            if root:
+                root.left, root.right = root.right, root.left
+                rec(root.left)
+                rec(root.right)
+
+        rec(root)
+        return root
