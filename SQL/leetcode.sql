4638150074a488b05ec0117effae63e5f297dd37
Ming <ming.qu0202@gmail.com>
2021-10-17 00:08:50 +0200

    sql

--- SQL/leetcode.sql
+++ SQL/leetcode.sql
@@ -40,4 +40,42 @@ So, if you have 3 items at rank 2, the next rank listed would be ranked 5.
 DENSE_RANK again gives you the ranking within your ordered partition, but the ranks are consecutive.
 */
 
+--178
+select score,
+	   dense_rank() over (order by score desc) as `rank`
+from scores;
+
+--180
+select  distinct first as consecutiveNums
+from(
+-- identify 3 consecutive ids in a row and their corresponding num
+select  A.num as first
+       ,B.num as second
+       ,C.num as third
+from logs A
+inner join logs B on A.id = B.id-1
+inner join logs C on A.id = C.id-2
+) tmp
+-- display num that appears at least three times consecutively
+where first = second and second = third and first = third
+
+--181
+SELECT e1.Name as Employee
+FROM Employee as e1
+JOIN Employee as e2
+ON  e1.ManagerId = e2.Id AND e1.Salary > e2.Salary;
+
+-- 182
+SELECT Email
+FROM Person
+GROUP BY Email
+HAVING COUNT(*) > 1;
+
+--183
+select Customers.Name as Customers
+from Customers
+left join Orders
+on Customers.Id = Orders.CustomerId
+where Orders.CustomerId is null;
+
 --
